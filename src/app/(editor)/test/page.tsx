d6b1bfd529ac171e00fcab4dd29a94a905d68c00
minkonkim <alsrhs98@gmail.com>
2025-07-19 15:07:07 +0900

    프로젝트 초기화

--- src/app/(editor)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SimpleTest } from "@/components/editor/simple-test";
-
-export default function TestPage() {
-  return (
-    <div className="w-full h-screen">
-      <SimpleTest />
-    </div>
-  );
-}
--- src/components/editor/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./test-control";
-export * from "./scene";
-export * from "./scene-object";
-export * from "./lighting";
-export * from "./grid";
-export * from "./camera-control";
